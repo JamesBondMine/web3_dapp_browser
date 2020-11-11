d9873480aaef2cb002de04185c6324478067e33b
JamesBondMine <lujia58@outlook.com>
2020-11-11 18:28:03 +0800

    此次内容

---
+++ Products/simple_rsa.framework/Headers/simple_rsa-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "SimpleRsaPlugin.h"
+
+FOUNDATION_EXPORT double simple_rsaVersionNumber;
+FOUNDATION_EXPORT const unsigned char simple_rsaVersionString[];
+
